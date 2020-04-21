ccf4766f5e1471101338ddb9516dfadf18a718a7
nathan mimoun <nathan.mimoun@gmail.com>
2020-04-21 19:16:19 +0200

    auth is working and location set for user

---
+++ authentification/admin.py
@@ -0,0 +1,16 @@
+from django.contrib.gis import admin
+from django.contrib.auth.models import Group
+# from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+class rien(admin.OSMGeoAdmin):
+    default_zoom = 15
+    default_lat = 6250868.90147960
+    default_lon = 255422.57162517
+    # modifiable = False
+    list_display = ('username','is_staff','is_active')
+    fields = ['username', ('first_name', 'last_name', 'email'), ('location', 'friends')]
+
+admin.site.register(User, rien)
+admin.site.unregister(Group)
---
+++ authentification/models.py
@@ -0,0 +1,11 @@
+from django.contrib.gis.db import models
+from django.contrib.auth.models import AbstractUser
+from PIL import Image
+
+
+class User(AbstractUser):
+    username = models.EmailField(max_length=254, unique=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics', verbose_name='')
+    location = models.PointField(null=True)
+    friends = models.ManyToManyField("User", related_name="is_friend_with")
+    REQUIRED_FIELDS = []
