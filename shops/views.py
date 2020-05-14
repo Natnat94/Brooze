aa5657708f50b3e91647179650ac4827eb87c212
nathan mimoun <nathan.mimoun@gmail.com>
2020-05-14 19:11:14 +0200

    api created

--- shops/views.py
+++ shops/views.py
@@ -1,15 +1,14 @@
 from django.contrib.gis.db.models.functions import Distance
-from django.contrib.gis.geos import GEOSGeometry, fromstr
+from django.contrib.gis.geos import GEOSGeometry, fromstr, Point
 from django.shortcuts import render
 from django.views import generic
 from django.core.serializers import serialize
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from authentification.models import User
+import json
 
 from .models import Shops
 from .engine import Matchmaker
-
-# Create your views here.
 
 
 class Home(generic.ListView):
@@ -18,22 +17,37 @@ class Home(generic.ListView):
 
     def get_queryset(self):
         user_location = User.objects.get(pk=1)
-        print(GEOSGeometry(user_location.location))
+        # print(GEOSGeometry(user_location.location))
         return Shops.objects.annotate(
-            distance=Distance('geom', user_location.location)
+            distance=Distance('geom', user_location.geom)
         ).order_by('distance')[0:6]
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        best_match = Matchmaker().find_shop()
+        best_match = Matchmaker().find_shop(id=1)
         data['result'] = Shops.objects.get(pk=best_match)
+        print(data['result'])
         return data
 
 
-def geolala(request):  # need to change the name of the method !!!
-    user_location = User.objects.get(pk=1)
-    data = Shops.objects.annotate(
-        distance=Distance('geom', user_location.location)
-    ).order_by('distance')[0:6]
-    rien = serialize('geojson', data)
-    return HttpResponse(rien, content_type='json')
+def api(request):
+    if request.method == 'POST':
+        user_location = User.objects.filter(pk=1)  # Selectionne l'utilisateur
+        data = json.loads(request.body)  # charge le contenu JSON de POST
+
+        pnt = Point(data['long'], data['lat'])  # Créé un point géographique pour Django
+        pnt = GEOSGeometry(pnt)
+        user_location.update(geom=pnt, last_name="mimoun")  # mets a jour sa position
+
+        best_match = Matchmaker().find_shop(id=1)
+        best_shop = Shops.objects.filter(pk=best_match)
+        response_data = serialize('geojson', best_shop)
+        return HttpResponse(response_data, content_type='json')
+
+    else:
+        user_location = User.objects.get(pk=1)  # give the last registered known location
+        data = Shops.objects.annotate(
+            distance=Distance('geom', user_location.geom)
+        ).order_by('distance')[0:30]
+        closest_shops = serialize('geojson', data)
+        return HttpResponse(closest_shops, content_type='json')
