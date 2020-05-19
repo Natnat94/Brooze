3e89a382c8adf768c59e7320f1c60896c7fd0222
nathan mimoun <nathan.mimoun@gmail.com>
2020-05-19 17:37:17 +0200

    changed to API compliant views

--- main/views.py
+++ main/views.py
@@ -1,34 +1,37 @@
-from django.core.serializers import serialize
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.gis.geos import GEOSGeometry, Point
 import json
+
+from django.contrib.gis.geos import GEOSGeometry, Point
+from django.core.serializers import serialize
+from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 from authentification.models import User
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, "main/index.html")
 
 
+@api_view()
+@permission_classes([IsAuthenticated])
 def user_detail(request, pk):
-    if request.method == 'GET':
-        user = User.objects.filter(pk=pk)
-        response_data = serialize('geojson', user)
-        return HttpResponse(response_data, content_type='json')
-    else:
-        return JsonResponse({'message': "erreur"})
+    user = User.objects.filter(pk=pk)
+    response_data = serialize("geojson", user)
+    return HttpResponse(response_data, content_type="json")
 
-@csrf_exempt
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def update_user_location(request, pk):
-    if request.method == 'POST':
-        user = User.objects.filter(pk=pk)  # Selectionne l'utilisateur
-        data = json.loads(request.body)  # charge le contenu JSON de POST
-        print(data)
-        pnt = Point(data['long'], data['lat'])  # Créé un point géographique pour Django
-        pnt = GEOSGeometry(pnt)
-        user.update(geom=pnt, last_name="mimoun")  # mets a jour sa position
-        response_data = serialize('geojson', user)
-        return HttpResponse(response_data, content_type='json')
-    else:
-        return JsonResponse({'message': "erreur"})
+    user = User.objects.filter(pk=pk)  # Selectionne l'utilisateur
+    data = json.loads(request.body)  # charge le contenu JSON de POST
+    # print(data)
+    pnt = Point(
+        data["long"], data["lat"]
+    )  # Créé un point géographique pour Django
+    pnt = GEOSGeometry(pnt)
+    user.update(geom=pnt, last_name="mimoun")  # mets a jour sa position
+    response_data = serialize("geojson", user)
+    return HttpResponse(response_data, content_type="json")
