5e835b009d3c580c7efbc4f8dba40d2147f7a06b
nathan mimoun <nathan.mimoun@gmail.com>
2020-05-25 14:53:18 +0200

    submit form bug fixed & profil view added

--- authentification/views.py
+++ authentification/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
+from django.core.serializers import serialize
 from .forms import UserRegisterForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 
 from django.contrib.auth import authenticate
@@ -15,6 +17,7 @@ from rest_framework.status import (
     HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND,
     HTTP_200_OK,
+    HTTP_201_CREATED,
 )
 from rest_framework.response import Response
 from authentification.models import User
@@ -27,7 +30,7 @@ def register(request):
     and return a succes message with a auth token.
     the keys needed are: 'username', 'password1, 'password2'
     where 'username' is valid email format"""
-    form = UserRegisterForm(request.POST)
+    form = UserRegisterForm(request.data)
     if form.is_valid():
         form.save()
         username = form.cleaned_data.get("username")
@@ -39,7 +42,7 @@ def register(request):
                 "message": f"l'utilisateur est créé pour {username}",
                 "token": token.key,
             },
-            status=HTTP_200_OK,
+            status=HTTP_201_CREATED,
         )
     else:
         return Response(
@@ -55,14 +58,15 @@ def login(request):
     user primay key, the username and the auth token.
     the keys needed are: 'username', 'password' """
     username = request.data.get("username")
+    print(username)
     password = request.data.get("password")
+    print(password)
     if username is None or password is None:
         return Response(
             {"error": "Please provide both username and password"},
             status=HTTP_400_BAD_REQUEST,
         )
     user = authenticate(username=username, password=password)
-    print(user)
     if not user:
         return Response(
             {"error": "Invalid Credentials"}, status=HTTP_404_NOT_FOUND
@@ -85,17 +89,19 @@ def logout(request):
     that is in this form: 'Token ~the token id~' """
     request.auth.delete()
     username = request.user.username
-    return Response({"message": f"{username} is logged out"})
+    return Response(
+        {"message": f"{username} is logged out"}, status=HTTP_200_OK
+    )
 
 
 @api_view(["POST"])
 @permission_classes((IsAuthenticated,))
 def change_password(request):
-    """ this function change the password of the user and return
-    a success message with a new auth token.
-    the keys needed are: 'old_password', 'new_password1', 'new_password2',
-    'Authorization' """
-    form = PasswordChangeForm(request.user, request.POST)
+    """ this function change the password of the authenticated user and
+    return a success message with a new auth token.
+    the keys needed are: 'old_password', 'new_password1',
+    'new_password2' """
+    form = PasswordChangeForm(request.user, request.data)
     if form.is_valid():
         user = form.save()
         request.auth.delete()
@@ -117,13 +123,23 @@ def change_password(request):
 @api_view(["POST", "GET"])
 @permission_classes((IsAuthenticated,))
 def profil(request):
+    """ The POST method update the profil of the authenticated
+    user and return it.
+    The key needed is: 'username'. (disabled)
+    The optional keys are: 'first_name', 'last_name', 'image'(file !).
+    The GET method return the authenticated user's profil """
     user = request.user
     if request.method == "POST":
-        form = UserUpdateForm(request.POST, request.FILES, instance=user)
-        print(form)
+        form = UserUpdateForm(request.data, request.FILES, instance=user)
         if form.is_valid():
             form.save()
-            return Response({"message": "ok"}, status=HTTP_200_OK,)
+            data = {
+                "username": user.username,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "image": user.image.url,
+            }
+            return Response(data, status=HTTP_200_OK)
         else:
             return Response(
                 form.errors.get_json_data(escape_html=False),
@@ -131,11 +147,14 @@ def profil(request):
             )
 
     else:
-        u_form = UserUpdateForm(
-            initial={
-                "username": user.username,
-                "first_name": user.first_name,
-                "last_name": user.last_name,
-            }
-        )
-        return Response({"message": "erreur"}, status=HTTP_200_OK,)
+        friends = []
+        for i in user.friends.all():
+            friends.append(i.username)
+        data = {
+            "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "image": user.image.url,
+            "friends": friends,
+        }
+        return Response(data, status=HTTP_200_OK)
