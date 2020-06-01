e6915290560f4a3818312d6b9948e964843c62b7
nathan mimoun <nathan.mimoun@gmail.com>
2020-06-01 02:27:57 +0200

    error fixed on model

--- shops/models.py
+++ shops/models.py
@@ -2,10 +2,10 @@ from django.contrib.gis.db import models
 
 
 class Shops(models.Model):
-    name = models.CharField(max_length=50)
-    amenity = models.CharField(max_length=15, null=True)
-    addrhousenumber = models.CharField(max_length=5)
-    addrstreet = models.CharField(max_length=255)
-    addrcity = models.CharField(max_length=25, null=True)
-    addrpostcode = models.CharField(max_length=5)
+    name = models.CharField(max_length=50, blank=True)
+    amenity = models.CharField(max_length=15, blank=True)
+    addrhousenumber = models.CharField(max_length=5, blank=True)
+    addrstreet = models.CharField(max_length=255, blank=True)
+    addrcity = models.CharField(max_length=25, blank=True)
+    addrpostcode = models.CharField(max_length=5, blank=True)
     geom = models.PointField()
