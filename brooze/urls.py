b9a582e6ddbd6d9e6e8880e0b9dc4ec775c651c8
nathan mimoun <nathan.mimoun@gmail.com>
2020-06-02 22:40:16 +0200

    bug fix

--- brooze/urls.py
+++ brooze/urls.py
@@ -19,10 +19,10 @@ from main import views as main
 
 
 urlpatterns = [
-    path('/api', main.index, name='home'),
-    path('/api/django/', include('rest_framework.urls')),
-    path('/api/admin/', admin.site.urls),
-    path('/api/auth/', include('authentification.urls')),
-    path('/api/map/', include('shops.urls')),
-    path('/api/user/', include('main.urls')),
+    path('api', main.index, name='home'),
+    path('api/django/', include('rest_framework.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/auth/', include('authentification.urls')),
+    path('api/map/', include('shops.urls')),
+    path('api/user/', include('main.urls')),
 ]
