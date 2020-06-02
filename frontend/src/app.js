b41a99e3f6845394073618ea6f07a14239a4482a
nathan mimoun <nathan.mimoun@gmail.com>
2020-06-02 16:38:02 +0200

    token is now by default null

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import NavBar from './navbar';
-import MapRenderer from './map';
 import Auth from './user_auth';
+import Map from './map';
 
-const token = "Token 8d3082a2926981efba07836f7c96ac3008d4ea58"
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             is_logged: false,
-            token: token,
+            token: null,
             mode: null,
         };
         this.handler = this.handler.bind(this)
@@ -27,7 +26,7 @@ class App extends React.Component {
             <>
                 <NavBar is_logged={this.state.is_logged} handler={this.handler} />
                 <div id="containeres">
-                    <MapRenderer {...this.state} />
+                    <Map {...this.state} />
                     {this.state.mode !== null ? <Auth handler={this.handler} mode={this.state.mode} /> : null}
                 </div>
             </>
