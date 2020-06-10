9e82f07cd146c53aae8bbfd019bbd314f4742755
nathan mimoun <nathan.mimoun@gmail.com>
2020-06-10 18:22:04 +0200

    friends list component added to index

--- frontend/src/user_auth/index.js
+++ frontend/src/user_auth/index.js
@@ -4,6 +4,7 @@ import './login.css';
 import LogIn from './components/login'
 import PasswordChange from './components/password';
 import SignUp from './components/sign_up';
+import Friends from './components/friends';
 
 
 class Auth extends React.Component {
@@ -13,10 +14,8 @@ class Auth extends React.Component {
             {this.props.mode === "password" ? 
             <PasswordChange {...this.props}/>
             :
-            (this.props.mode === 'login' ?  <LogIn {...this.props} /> : <SignUp {...this.props}/>)}
-            
-            
-            
+            (this.props.mode === 'login' ?  <LogIn {...this.props} /> : ( 
+                this.props.mode === 'friends' ? <Friends {...this.props} /> : <SignUp {...this.props}/>))}
             </>
         );
     }
