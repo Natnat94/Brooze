2c261af822e4e931f81c2ea58a37f13f2489ecbf
nathan mimoun <nathan.mimoun@gmail.com>
2020-12-15 22:32:44 +0100

    small changes added

--- brooze/settings/__init__.py
+++ brooze/settings/__init__.py
@@ -173,9 +173,6 @@ CORS_ORIGIN_WHITELIST = (
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = [
-    "www.nathan-mimoun.live",
-    "nathan-mimoun.live",
-    "projet13.nathan-mimoun.live",
     "localhost",
     "127.0.0.1",
     "192.168.1.41",
@@ -196,7 +193,7 @@ REST_FRAMEWORK = {
 NOSE_ARGS = [
     '--with-coverage',
     '--cover-package=authentification, main, shops',
-    '--cover-html',
+    '--cover-html'
 ]
 
 
@@ -220,3 +217,39 @@ EMAIL_PORT = '2525'
 ENVIRONMENT_FLOAT = True
 ENVIRONMENT_NAME = "Development server"
 ENVIRONMENT_COLOR = "#1dc022"
+
+
+#########################
+#   SimpleJWT setting   #
+#########################
+
+from datetime import timedelta
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
