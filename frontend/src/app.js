e1c8f6f7d30183a0c170cc2785cba7ca6e1caef3
nathan mimoun <nathan.mimoun@gmail.com>
2020-12-15 19:09:19 +0100

    switched to env var for base url

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,19 +1,13 @@
+/* eslint-disable */
 import React from 'react';
-import NavBar from './navbar';
-import Auth from './user_auth';
-import Map from './map';
+import { NavBar } from './navbar';
+import MobileNav from './navbar';
 import SnackBar from './notification_toast/snackbar';
+import Main from './main'
 
-let mainurl
+
 let temptoken
-temptoken = null
-mainurl = 'https://nathan-mimoun.live/api'
-// mainurl =  'http://localhost:8000'
-// if (process.env.NODE_ENV === 'production') {mainurl =  'https://nathan-mimoun.live/api'
-// temptoken = null
-// } else {mainurl =  'http://localhost:8000'
-// temptoken = "Token 825e04c5a051b03a208ea6baec3b3478ad348067"
-// }
+temptoken = "Token ea6bab4b6f7225985f239e7f665a6a752609e14a"
 
 class App extends React.Component {
     constructor(props) {
@@ -21,8 +15,8 @@ class App extends React.Component {
         this.state = {
             is_logged: false,
             token: temptoken,
-            mode: null,
-            mainurl: mainurl,
+            mode: "home",
+            mainurl: process.env.REACT_APP_BASE_URL,
             snackbar: false,
             snackbar_text: null
         };
@@ -34,24 +28,34 @@ class App extends React.Component {
         this.setState({
             [name]: value,
         })
-  
     }
-    
 
-    render() { 
-        if (this.state.is_logged === false) { 
-            if (localStorage.getItem('token')) {
-                this.setState({token: 'Token ' + localStorage.getItem('token'), is_logged: true})
-            }
+    componentDidMount() {
+
+        // check if the token is already saved into the local storage if the user is not logged
+        if ((!this.state.is_logged) && (localStorage.getItem('token'))) {
+            this.setState({ token: localStorage.getItem('token'), is_logged: true })
         }
+    }
+
+    render() {
+        // definition of the max width for a mobile
+        const is_mobile = window.innerWidth < 780
+
+        // check if its a mobile screen or not and render the adequate nav. menu
+        const navigation = is_mobile ? <MobileNav {...this.state} handler={this.handler} /> : <NavBar {...this.state} handler={this.handler} />;
+
+        // check if this is a mobile viewport (screen width) else return an error message
+        if (!is_mobile) { return (<div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>This app is meant to be rendered on a mobile screen only</div>) }
+
+        // check if the device have a geolocation capability else return an error message 
+        if (!navigator.geolocation) { return (<div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>This app need a geolocation capability on your device</div>) }
+
         return (
             <>
                 <SnackBar snackbar={this.state.snackbar} text={this.state.snackbar_text} handler={this.handler} />
-                <NavBar {...this.state} handler={this.handler} />
-                <div id="main-container">
-                    <Map {...this.state} mainurl={mainurl} />
-                    {this.state.mode !== null ? <Auth handler={this.handler} {...this.state} /> : null}
-                </div>
+                <Main {...this.state} handler={this.handler} />
+                {navigation}
             </>
         );
     }
