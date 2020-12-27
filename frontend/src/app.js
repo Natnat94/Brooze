6fb0bb209be72149a5c2abbe1a2f92f2d3845053
nathan mimoun <nathan.mimoun@gmail.com>
2020-12-27 23:56:02 +0100

    New snackbar added, state handle metho. change

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,9 @@
 /* eslint-disable */
 import React from 'react';
-import { NavBar } from './navbar';
-import MobileNav from './navbar';
-import SnackBar from './notification_toast/snackbar';
+import MobileNav,{ NavBar } from './navbar';
 import Main from './main'
 import { postData } from './main/map/ApiDataFunc'
+import SnackBar from '@material-ui/core/Snackbar';
 
 class App extends React.Component {
     constructor(props) {
@@ -14,21 +13,36 @@ class App extends React.Component {
             token: null,
             mode: "home",
             mainurl: process.env.REACT_APP_BASE_URL,
-            snackbar: false,
             snackbar_text: null,
-            error_access: false
+            error_access: false,
+            snackbar_is_open: false
         };
-
-        this.handler = this.handler.bind(this)
+        this.handleModeChange = this.handleModeChange.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+        this.handleSnackbar = this.handleSnackbar.bind(this);
+        this.handleErrorAccess = this.handleErrorAccess.bind(this);
     }
 
-    handler(name, value) {
+    handleSnackbar(message) {
+        this.setState({ snackbar_is_open: true, snackbar_text: message });
+    };
+
+    handleModeChange(newMode) {
+        this.setState({ mode: newMode });
+    }
+
+    handleLogout() {
+        localStorage.removeItem('refresh_token');
+        this.setState({ is_logged: false, refresh_token: null })
+    }
+
+    handleErrorAccess() {
         this.setState({
-            [name]: value,
+            error_access: true
         })
     }
-    componentDidMount() {
 
+    componentDidMount() {
         // check if the token is already saved into the local storage if the user is not logged
         if ((!this.state.is_logged) && (localStorage.getItem('refresh_token'))) {
             this.setState({ refresh_token: localStorage.getItem('refresh_token'), is_logged: true })
@@ -42,8 +56,8 @@ class App extends React.Component {
                 let token = { refresh: this.state.refresh_token }
                 postData(this.state.mainurl + '/auth/token/refresh/', token, '')
                     .then(result => this.setState({ token: "Bearer " + result.access }))
-                    .catch(error => { localStorage.removeItem('refresh_token'); this.setState({ is_logged: false })})
-                this.setState({error_access: false})
+                    .catch(error => { localStorage.removeItem('refresh_token'); this.setState({ is_logged: false }) })
+                this.setState({ error_access: false })
             }
         }
     }
@@ -53,7 +67,7 @@ class App extends React.Component {
         const is_mobile = window.innerWidth < 780
 
         // check if its a mobile screen or not and render the adequate nav. menu
-        const navigation = is_mobile ? <MobileNav {...this.state} handler={this.handler} /> : <NavBar {...this.state} handler={this.handler} />;
+        const navigation = is_mobile ? <MobileNav is_logged={this.state.is_logged} token={this.state.token} mainurl={this.state.mainurl} onModeChange={this.handleModeChange} snackBar={this.handleSnackbar} logOut={this.handleLogout} /> : <NavBar {...this.state} onModeChange={this.handleModeChange} snackBar={this.handleSnackbar} />;
 
         // check if this is a mobile viewport (screen width) else return an error message
         if (!is_mobile) { return (<div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>This app is meant to be rendered on a mobile screen only</div>) }
@@ -63,9 +77,20 @@ class App extends React.Component {
 
         return (
             <>
-                <SnackBar snackbar={this.state.snackbar} text={this.state.snackbar_text} handler={this.handler} />
-                <Main {...this.state} handler={this.handler} />
+                <Main {...this.state} handleErrorAccess={this.handleErrorAccess} onModeChange={this.handleModeChange} snackBar={this.handleSnackbar}/>
                 {navigation}
+                <div>
+                    <SnackBar
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={this.state.snackbar_is_open}
+                        autoHideDuration={4000}
+                        onClose={() => this.setState({ snackbar_is_open: false, snackbar_text: null })}
+                        message= {this.state.snackbar_text}
+                    />
+                </div>
             </>
         );
     }
